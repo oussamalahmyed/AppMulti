b6143010adfd7d1309d3ae183edef5d49391f0fb
Oussama Lahmyed <oussamalahmyed@gmail.com>
2023-01-28 02:44:54 +0100

    Task07: Bug Fixed in Master

--- app/src/main/java/com/example/application/Main.java
+++ app/src/main/java/com/example/application/Main.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
     Button btn_quitter,btn_reinitialiser;
     EditText Number;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class Main extends AppCompatActivity {
         btn_quitter = findViewById(R.id.btnquitter);
         btn_reinitialiser=findViewById(R.id.btnreinitialiser);
         Number=findViewById(R.id.txtNumber);
+        tv=findViewById(R.id.tview);
 
         btn_quitter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,7 @@ public class Main extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Number.setText(null);
+                tv.setText("? * 1 =?\n ? * 2 =?\n ? * 3 =?\n ? * 4 =?\n ? * 5 =?\n ? * 6 =?\n ? * 7 =?\n ? * 8 =?\n ? * 9 =?\n ? * 10 =?");
             }
         });
     }
